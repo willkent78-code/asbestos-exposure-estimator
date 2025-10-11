7499f4711caeb77fb181aadd0a8cfa7597f39950
willkent78-code <willkent78@gmail.com>
2025-10-11 20:42:35 +0100

    Add waitress runner

--- run_waitress.py
+++ run_waitress.py
@@ -1,8 +1,6 @@
-@'
 import os
 from waitress import serve
-from app import app   # imports `app` from app.py
+from app import app  # if your main file is app.py; otherwise: from app_flask_backup import app
 
 port = int(os.environ.get("PORT", "8000"))
 serve(app, host="0.0.0.0", port=port)
-'@ | Set-Content .\run_waitress.py -NoNewline
